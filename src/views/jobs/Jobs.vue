bf420913e3e2d31ff39bb56f7557e5055fe20895
beltranbot <beltranbot@gmail.com>
2021-06-03 10:28:49 -0500

    dynamic links

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-      <h2>{{job.title}}</h2>
+  <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+          <h2>{{ job.title }}</h2>
+      </router-link>
   </div>
 </template>
 
@@ -12,7 +14,7 @@ export default {
             jobs: [
                 { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
                 { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-                { title: 'Ninja Vue Developer', id: 1, details: 'lorem' },
+                { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
             ]
         }
     }
@@ -21,5 +23,21 @@ export default {
 </script>
 
 <style>
+.job h2 {
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+}
 
+.job h2:hover {
+    background: #ddd;
+}
+
+.job a {
+    text-decoration: none;
+}
 </style>
