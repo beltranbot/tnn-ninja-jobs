96cb5344343bb316011c0da06dd2b0a0d24088d9
beltranbot <beltranbot@gmail.com>
2021-06-03 11:23:24 -0500

    fetching data from components, conditionally showing data

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,24 +1,29 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
       <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-          <h2>{{ job.title }}</h2>
+        <h2>{{ job.title }}</h2>
       </router-link>
+    </div>
   </div>
+  <div v-else>loading jobs...</div>
+  
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            jobs: [
-                { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
-                { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-                { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
-            ]
-        }
+  data() {
+    return {
+      jobs: []
     }
-
+  },
+  mounted() {
+    fetch("http://localhost:4000/jobs")
+      .then(res => res.json())
+      .then(data => this.jobs = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
 
